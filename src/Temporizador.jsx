8e5d88b65dbb231271ad3bfe7079d7f8ca0b2a54
jovanny <jyokari@gmail.com>
2023-01-05 12:40:00 -0600

    Avance del disenio del temporizador

--- src/Temporizador.jsx
+++ src/Temporizador.jsx
@@ -1,9 +1,19 @@
 import reloj from './assets/cronometro.png'
+import './css/temporizador.css'
 
 const Temporizador = () => {
     return (
         <div className='container'>
-            <div className="temporizador"></div>
+            <div className="temporizador">
+                <div className="contentDatos">
+                    <label htmlFor="">00</label>
+                    <label htmlFor="">:</label>
+                    <label htmlFor="">00</label>
+                    <label htmlFor="">:</label>
+                    <label htmlFor="">00</label>
+                </div>
+            </div>
+            <div className="config"></div>
         </div>
     )
 }
