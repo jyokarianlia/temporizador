7a1cd84411405dbc30ae5e64bdb981b40fed749d
kikin-mentor <git.mentor.2022@gmail.com>
2023-01-05 12:40:56 -0600

    crono

--- src/Cronometro.jsx
+++ src/Cronometro.jsx
@@ -1,8 +1,27 @@
+import "./css/cronometro.css";
 const Cronometro = () => {
 
     return (
         <div>
-            <h2>cronometro</h2>
+            <h2>Cronometro</h2>
+            <section>
+                <div className="cuadro">
+                    <div className="cuadro__num">00</div>
+                    <div className="cuadro__dhms">DAYS</div>
+                </div>
+                <div className="cuadro">
+                    <div className="cuadro__num">00</div>
+                    <div className="cuadro__dhms">HOURS</div>
+                </div>
+                <div className="cuadro">
+                    <div className="cuadro__num">00</div>
+                    <div className="cuadro__dhms">MINUTES</div>
+                </div>
+                <div className="cuadro">
+                    <div className="cuadro__num">00</div>
+                    <div className="cuadro__dhms">SECONDS</div>
+                </div>
+            </section>
         </div>
     )
 }
