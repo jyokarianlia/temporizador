af16e384f2c7313f3052b85cf6116e026842fb03
kikin-mentor <git.mentor.2022@gmail.com>
2023-01-05 13:43:50 -0600

    crono_buttons

--- src/Cronometro.jsx
+++ src/Cronometro.jsx
@@ -4,23 +4,28 @@ const Cronometro = () => {
     return (
         <div>
             <h2>Cronometro</h2>
-            <section>
+            <section className="main">
                 <div className="cuadro">
                     <div className="cuadro__num">00</div>
-                    <div className="cuadro__dhms">DAYS</div>
+                    <div className="cuadro__dhms"><span>DAYS</span></div>
                 </div>
                 <div className="cuadro">
                     <div className="cuadro__num">00</div>
-                    <div className="cuadro__dhms">HOURS</div>
+                    <div className="cuadro__dhms"><span>HOURS</span></div>
                 </div>
                 <div className="cuadro">
                     <div className="cuadro__num">00</div>
-                    <div className="cuadro__dhms">MINUTES</div>
+                    <div className="cuadro__dhms"><span>MINUTES</span></div>
                 </div>
                 <div className="cuadro">
                     <div className="cuadro__num">00</div>
-                    <div className="cuadro__dhms">SECONDS</div>
+                    <div className="cuadro__dhms"><span>SECONDS</span></div>
                 </div>
+            </section>
+            <section className="actions-btns">
+                <button className="ini">INICIAR</button>
+                <button className="det">DETENER</button>
+                <button className="rei">REINICIAR</button>
             </section>
         </div>
     )
