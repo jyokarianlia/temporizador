db335c64fc456bb72782fa2831ec1d31b104eb5c
kikin-mentor <git.mentor.2022@gmail.com>
2023-01-05 14:02:20 -0600

    crono_ btn-refresh

--- src/Cronometro.jsx
+++ src/Cronometro.jsx
@@ -6,20 +6,20 @@ const Cronometro = () => {
             <h2>Cronometro</h2>
             <section className="main">
                 <div className="cuadro">
-                    <div className="cuadro__num">00</div>
-                    <div className="cuadro__dhms"><span>DAYS</span></div>
-                </div>
-                <div className="cuadro">
-                    <div className="cuadro__num">00</div>
+                    <div className="cuadro__num h">00</div>
                     <div className="cuadro__dhms"><span>HOURS</span></div>
                 </div>
                 <div className="cuadro">
-                    <div className="cuadro__num">00</div>
+                    <div className="cuadro__num m">00</div>
                     <div className="cuadro__dhms"><span>MINUTES</span></div>
                 </div>
                 <div className="cuadro">
-                    <div className="cuadro__num">00</div>
+                    <div className="cuadro__num s">00</div>
                     <div className="cuadro__dhms"><span>SECONDS</span></div>
+                </div>
+                <div className="cuadro">
+                    <div className="cuadro__num ms">00</div>
+                    <div className="cuadro__dhms"><span>MILISECONDS</span></div>
                 </div>
             </section>
             <section className="actions-btns">
