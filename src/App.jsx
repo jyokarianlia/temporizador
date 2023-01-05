a95a7a7ce0fad7c43cf141dbc5c9574942969bfd
kikin-mentor <git.mentor.2022@gmail.com>
2023-01-05 11:36:43 -0600

    vista del cronometro

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import Cronometro from './Cronometro'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Cronometro />
     </div>
   )
 }
--- src/Cronometro.jsx
+++ src/Cronometro.jsx
@@ -2,7 +2,7 @@ const Cronometro = () => {
 
     return (
         <div>
-            
+            <h2>cronometro</h2>
         </div>
     )
 }
